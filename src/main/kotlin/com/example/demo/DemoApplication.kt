64823215836a8f3ab8be422c2810f068f99fdf8e
Michel Jung <michel.jung89@gmail.com>
2024-03-21 20:54:31 +0100

    Reproduce InaccessibleObjectException

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,10 +2,25 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.keyvalue.annotation.KeySpace
+import org.springframework.data.map.repository.config.EnableMapRepositories
+import org.springframework.data.repository.CrudRepository
+import java.net.URL
 
 @SpringBootApplication
+@EnableMapRepositories
 class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
+
+interface WebsiteRepository : CrudRepository<Website, String>
+
+@KeySpace("website")
+data class Website(
+    @Id
+    val id: String,
+    val url: URL,
+)
